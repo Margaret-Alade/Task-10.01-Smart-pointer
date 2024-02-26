f03172490f05f90325d68813beee8c01744a9d56
Margaret Alade <alade.margaret@yandex.ru>
2024-02-26 06:42:28 +0300

    Initial Commit

---
+++ Task-10.01 Smart pointer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Task-10.01 Smart pointer
+//
+//  Created by Маргарет  on 26.02.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
